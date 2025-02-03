06c36ae1ca19ac5b9342043561ef42d90fdf2222
kenichi-maeda <kenichi.maeda121@gmail.com>
2025-02-02 21:09:42 -0500

    Update utils

--- utils.py
+++ utils.py
@@ -8,6 +8,8 @@ import os
 from scipy.spatial import KDTree
 import trimesh
 from scipy.spatial import cKDTree
+from numpy.linalg import norm
+import pymeshfix
 
 def process_mesh(mesh, mesh_processing_function):
     vertices_before = mesh.vertices
@@ -162,6 +164,98 @@ def visualize_two_meshes(mesh1, mesh2, filename1="Mesh 1", filename2="Mesh 2"):
     plotter.add_legend()
     display(plotter.show(jupyter_backend="trame"))
 
+def visualize_three_meshes(mesh1, mesh2, mesh3, filename1="Mesh 1", filename2="Mesh 2", filename3="Mesh 3"):
+    mesh1 = convert_to_pyvista(mesh1)
+    mesh2 = convert_to_pyvista(mesh2)
+    mesh3 = convert_to_pyvista(mesh3)
+    
+    plotter = pv.Plotter(shape=(1, 3), notebook=True)
+
+    # Mesh 1
+    plotter.subplot(0, 0)
+    mesh_actor1 = plotter.add_mesh(
+        mesh1,
+        color="white",
+        show_edges=True,
+        edge_color="black",
+        label=filename1,
+        line_width=0.3
+    )
+    plotter.add_text("Mesh 1", position="upper_left", font_size=10)
+    
+    def update_clipping_plane_mesh1(value):
+        new_plane = pv.Plane(center=(value, 0, 0), direction=(1, 0, 0))
+        clipped_mesh = mesh1.clip_surface(new_plane, invert=False)
+        mesh_actor1.mapper.SetInputData(clipped_mesh)
+        plotter.render()
+    
+    plotter.add_slider_widget(
+        callback=update_clipping_plane_mesh1,
+        rng=[-2, 2],
+        value=0,
+        title="Clip Plane Mesh 1",
+        pointa=(0.2, 0.9),
+        pointb=(0.8, 0.9)
+    )
+
+    # Mesh 2
+    plotter.subplot(0, 1)
+    mesh_actor2 = plotter.add_mesh(
+        mesh2,
+        color="white",
+        show_edges=True,
+        edge_color="black",
+        label=filename2,
+        line_width=0.3
+    )
+    plotter.add_text("Mesh 2", position="upper_left", font_size=10)
+    
+    def update_clipping_plane_mesh2(value):
+        new_plane = pv.Plane(center=(value, 0, 0), direction=(1, 0, 0))
+        clipped_mesh = mesh2.clip_surface(new_plane, invert=False)
+        mesh_actor2.mapper.SetInputData(clipped_mesh)
+        plotter.render()
+    
+    plotter.add_slider_widget(
+        callback=update_clipping_plane_mesh2,
+        rng=[-2, 2],
+        value=0,
+        title="Clip Plane Mesh 2",
+        pointa=(0.2, 0.9),
+        pointb=(0.8, 0.9)
+    )
+    
+    # Mesh 3
+    plotter.subplot(0, 2)
+    mesh_actor3 = plotter.add_mesh(
+        mesh3,
+        color="white",
+        show_edges=True,
+        edge_color="black",
+        label=filename3,
+        line_width=0.3
+    )
+    plotter.add_text("Mesh 3", position="upper_left", font_size=10)
+    
+    def update_clipping_plane_mesh3(value):
+        new_plane = pv.Plane(center=(value, 0, 0), direction=(1, 0, 0))
+        clipped_mesh = mesh3.clip_surface(new_plane, invert=False)
+        mesh_actor3.mapper.SetInputData(clipped_mesh)
+        plotter.render()
+    
+    plotter.add_slider_widget(
+        callback=update_clipping_plane_mesh3,
+        rng=[-2, 2],
+        value=0,
+        title="Clip Plane Mesh 3",
+        pointa=(0.2, 0.9),
+        pointb=(0.8, 0.9)
+    )
+    
+    plotter.add_legend()
+    display(plotter.show(jupyter_backend="trame"))
+
+
 def visualize_intersection(mesh, filename="Processed Mesh"):
     intersections = pymesh.detect_self_intersection(mesh)
     intersecting_faces = set(intersections.flatten())
@@ -211,7 +305,7 @@ def intermediate(mesh):
     
     if os.path.exists(temp_file):
         os.remove(temp_file)
-    
+        
     return mesh
 
 def evaluation(mesh1, mesh2):
@@ -228,6 +322,29 @@ def evaluation(mesh1, mesh2):
         ["Number of intersecting face pairs", len(before_intersections), len(after_intersections)],
         ["Volume", before_trimesh.volume, after_trimesh.volume],
         ["Area", before_trimesh.area, after_trimesh.area]
+    ]
+
+    print(tabulate(table_data, headers="firstrow", tablefmt="grid"))
+
+def full_evaluation(original, mesh1, mesh2, mesh3):
+    before_trimesh = trimesh.Trimesh(vertices=original.vertices, faces=original.faces)
+    after_trimesh1 = trimesh.Trimesh(vertices=mesh1.vertices, faces=mesh1.faces)
+    after_trimesh2 = trimesh.Trimesh(vertices=mesh2.vertices, faces=mesh2.faces)
+    after_trimesh3 = trimesh.Trimesh(vertices=mesh3.vertices, faces=mesh3.faces)
+
+    before_intersections = pymesh.detect_self_intersection(original)
+    after_intersections1 = pymesh.detect_self_intersection(mesh1)
+    after_intersections2 = pymesh.detect_self_intersection(mesh2)
+    after_intersections3 = pymesh.detect_self_intersection(mesh3)
+
+    table_data = [
+        ["Metric", "Original", "Method1", "Method2", "Method3"],
+        ["Number of vertices", len(original.vertices), len(mesh1.vertices), len(mesh2.vertices), len(mesh3.vertices)],
+        ["Number of faces", len(original.faces),len(mesh1.faces), len(mesh2.faces), len(mesh3.faces)],
+        ["Number of intersecting face pairs", len(before_intersections), len(after_intersections1), len(after_intersections2), len(after_intersections3)],
+        ["Volume", before_trimesh.volume, after_trimesh1.volume, after_trimesh2.volume, after_trimesh3.volume],
+        ["Area", before_trimesh.area, after_trimesh1.area, after_trimesh2.area, after_trimesh3.area],
+        ["Mean displacement", "NaN", _evaluate_displacement(original, mesh1), _evaluate_displacement(original, mesh2), _evaluate_displacement(original, mesh3) ]
     ]
 
     print(tabulate(table_data, headers="firstrow", tablefmt="grid"))
@@ -309,7 +426,7 @@ def detect_boundary_vertices(mesh):
     return boundary_mask
 
 
-def align_submesh_boundary(remaining_mesh, repaired_submesh, tolerance=1):
+def align_submesh_boundary(remaining_mesh, repaired_submesh, tolerance=3):
     """
     Align the boundary vertices of the repaired submesh to the original mesh's boundary.
     """
@@ -350,6 +467,32 @@ def replace_submesh_in_original(remaining_mesh, repaired_submesh):
     merged, _ = pymesh.remove_duplicated_faces(merged)
     merged, _ = pymesh.remove_isolated_vertices(merged)
     return merged
+
+def _evaluate_displacement(mesh1, mesh2):
+
+    original_vertices = mesh1.vertices
+    repaired_vertices = mesh2.vertices
+
+    # Build KD-Trees for efficient nearest-neighbor search
+    original_tree = cKDTree(original_vertices)
+    repaired_tree = cKDTree(repaired_vertices)
+
+    # For each vertex in the original mesh, the distance to the nearest vertex in the repaired mesh.
+    distances_original_to_repaired, _ = original_tree.query(repaired_vertices)
+
+    # For each vertex in the repaired mesh, the distance to the nearest vertex in the original mesh.
+    distances_repaired_to_original, _ = repaired_tree.query(original_vertices)
+
+    # Combine distances for bidirectional matching
+    # Why?
+    # - Some vertices in the original mesh may not have a corresponding counterpart in the repaired mesh (e.g., due to merging or removal).
+    # - Similarly, new vertices in the repaired mesh may not have counterparts in the original mesh (e.g., due to splitting or addition).
+    all_distances = np.concatenate([distances_original_to_repaired, distances_repaired_to_original])
+
+    max_displacement = np.max(all_distances)
+    mean_displacement = np.mean(all_distances)
+
+    return mean_displacement
 
 
 def evaluate_displacement(mesh1, mesh2):
@@ -517,5 +660,123 @@ def refinement(mesh):
     mesh, __ = pymesh.collapse_short_edges(mesh, 1e-6)
     mesh, __ = pymesh.remove_obtuse_triangles(mesh, 175.0, 10)
     mesh, _ = pymesh.remove_isolated_vertices(mesh)
-    mesh = pymesh.resolve_self_intersection(mesh)
+    while len(pymesh.detect_self_intersection(intermediate(mesh))) != 0:
+        mesh = pymesh.resolve_self_intersection(intermediate(mesh))
     return mesh
+
+def iterative_repair(mesh, with_rounding=True, precision=11, max_iterations=15):
+    """
+    Resolves self-intersections in a mesh iteratively.
+
+    Parameters:
+        mesh: The input mesh object.
+        with_rounding (bool): Enables rounding of vertices for stability. Default is True.
+        precision (int): Rounding precision level for vertices.
+        max_iterations (int): Maximum number of iterations allowed to resolve intersections.
+
+    Returns:
+        mesh: The processed mesh with no self-intersections.
+    """
+    # Initial rounding of vertices
+    if (with_rounding):
+        mesh = pymesh.form_mesh(
+                np.round(mesh.vertices, precision),
+                mesh.faces);
+    intersecting_faces = pymesh.detect_self_intersection(mesh);
+
+    # Iterative process to resolve self-intersections
+    counter = 0;
+    while len(intersecting_faces) > 0 and counter < max_iterations:
+        if (with_rounding):
+            involved_vertices = np.unique(mesh.faces[intersecting_faces].ravel());
+
+            # Round only the involved vertices
+            # Suppose precision = 4. Then,
+            # [1.234567, 2.345678, 3.456789] <- One vertex example (x, y, z coords)
+            # becomes
+            # [1.23, 2.35, 3.46]
+            vertices_copy = mesh.vertices.copy()  
+            vertices_copy[involved_vertices, :] =\
+                    np.round(mesh.vertices[involved_vertices, :],
+                            precision//2);
+        
+            mesh = pymesh.form_mesh(vertices_copy, mesh.faces) 
+
+        mesh = pymesh.resolve_self_intersection(mesh, "igl");
+        mesh, __ = pymesh.remove_duplicated_faces(mesh, fins_only=True);
+        if (with_rounding):
+            mesh = pymesh.form_mesh(
+                    np.round(mesh.vertices, precision),
+                    mesh.faces);
+        mesh = intermediate(mesh) # Reload mesh. Otherwise, the next step fails in some cases.
+        intersecting_faces = pymesh.detect_self_intersection(mesh);
+        print(len(intersecting_faces))
+        counter += 1;
+
+    if len(intersecting_faces) > 0:
+        logging.warn("Resolving failed: max iteration reached!");
+
+    return mesh
+
+def remesh(mesh, detail=2.4e-2):
+
+    bbox_min, bbox_max = mesh.bbox
+    diag_len = norm(bbox_max - bbox_min)
+    target_len = diag_len * detail
+    #print("Target resolution: {} mm".format(target_len))
+
+    count = 0
+    mesh, __ = pymesh.remove_degenerated_triangles(mesh, 100)
+    mesh, __ = pymesh.split_long_edges(mesh, target_len)
+    num_vertices = mesh.num_vertices
+    while True:
+        mesh, __ = pymesh.collapse_short_edges(mesh, 1e-6, preserve_feature=True) # Remove extremely small edges
+        mesh, __ = pymesh.collapse_short_edges(mesh, target_len,
+                                               preserve_feature=True)
+        mesh, __ = pymesh.remove_obtuse_triangles(mesh, 150.0, 100)
+        if mesh.num_vertices == num_vertices:
+            break
+
+        num_vertices = mesh.num_vertices
+        #print("#v: {}".format(num_vertices))
+        count += 1
+        if count > 10: break
+
+    mesh = pymesh.resolve_self_intersection(mesh)
+    mesh, __ = pymesh.remove_duplicated_faces(mesh)
+    mesh = pymesh.compute_outer_hull(mesh)
+    mesh, __ = pymesh.remove_duplicated_faces(mesh)
+    mesh, __ = pymesh.remove_obtuse_triangles(mesh, 179.9, 5)
+    mesh, __ = pymesh.remove_isolated_vertices(mesh)
+
+    return mesh
+
+def direct_repair(mesh):
+    vertices = mesh.vertices.copy()
+    faces = mesh.faces.copy()
+    clean_vertices, clean_faces = pymeshfix.clean_from_arrays(vertices, faces)
+    return pymesh.form_mesh(clean_vertices, clean_faces)
+
+def repair_with_local_remesh(mesh):
+    intersections = pymesh.detect_self_intersection(mesh)
+    intersecting_vertices, intersecting_faces = track_self_intersecting_faces(mesh, intersections)
+    outer_hull = pymesh.compute_outer_hull(mesh)
+    mapped_vertices = map_to_modified_mesh(mesh, outer_hull, intersecting_vertices)
+    submesh, face_mask = extract_self_intersecting_region_from_modified(outer_hull, mapped_vertices)
+    remaining_mesh = extract_remaining_mesh(outer_hull, face_mask)
+
+    components = pymesh.separate_mesh(submesh)
+
+    if len(components) == 1:
+        repaired_submesh = remesh(submesh)
+    else:
+        repaired_components = []
+        for compoenent in components:
+            repaired_component = remesh(compoenent)
+            repaired_components.append(repaired_component)
+        repaired_submesh = pymesh.merge_meshes(repaired_components)
+
+    aligned_submesh = align_submesh_boundary(remaining_mesh, repaired_submesh)
+    repaired_full = replace_submesh_in_original(remaining_mesh, aligned_submesh)
+    final = refinement(repaired_full)
+    return final
